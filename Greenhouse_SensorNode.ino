989383f2e7c6a2c298a5a8570ee85e43484dace7
john-tng1 <13621242@student.uts.edu.au>
2021-04-08 21:55:08 +1000

    Refactor code from loop to readSensor()

--- Greenhouse_SensorNode.ino
+++ Greenhouse_SensorNode.ino
@@ -16,6 +16,10 @@
 #define DHTTYPE DHT22 // DHT 22  (AM2302)
 
 DHT dht(DHTPIN, DHTTYPE);
+// Declare value for humidity and temperature
+float h = 0;
+float t = 0;
+int m = analogRead(A1);
 
 // Configure network here
 WiFiClient wifiClient;
@@ -96,6 +100,50 @@ void connectMQTT()
     client.subscribe("Outside/Greenhouse");
   }
 }
+
+void readSensor()
+{
+  h = dht.readHumidity();
+  // Read temperature as Celsius
+  t = dht.readTemperature();
+  
+  /* Read mosture sensor value
+    The sensor value description
+    # 0  ~300     dry soil
+    # 300~700     humid soil
+    # 700~950     in water
+  */
+  String soilMoisture = "";
+  if (m > 700)
+  {
+    soilMoisture = String(m) + ", in water";
+  }
+  else if (m > 300)
+  {
+    soilMoisture = String(m) + ", humid soil";
+  }
+  else
+  {
+    soilMoisture = String(m) + ", dry soil";
+  }
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t))
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C ");
+  Serial.print("Soil Moisture Value: ");
+  Serial.println(soilMoisture);
+}
+
 void setup()
 {
   Serial.begin(9600);
@@ -125,47 +173,6 @@ void loop()
     Serial.println("MQTT connection lost - trying again.");
     connectMQTT();
   }
-
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
-  // Read temperature as Celsius
-  float t = dht.readTemperature();
-  /* Read mosture sensor value
-    The sensor value description
-    # 0  ~300     dry soil
-    # 300~700     humid soil
-    # 700~950     in water
-  */
-
-  String soilMoisture = "";
-  int m = analogRead(A1);
-  if (m > 700)
-  {
-    soilMoisture = String(m) + ", in water";
-  }
-  else if (m > 300)
-  {
-    soilMoisture = String(m) + ", humid soil";
-  }
-  else
-  {
-    soilMoisture = String(m) + ", dry soil";
-  }
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t))
-  {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.println(" *C ");
-  Serial.print("Soil Moisture Value: ");
-  Serial.println(soilMoisture);
+  
+  readSensor();
 }
